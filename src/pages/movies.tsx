796da53920caffd390f28bd38a275d9a4fbfeba5
avaishnavi03 <avaishnavi.intern@infomericainc.in>
2026-01-12 15:58:01 +0530

    filter dropdown

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -2,9 +2,19 @@ import { useEffect, useState } from "react";
 import MovieCard from "../components/ui/button/movieCard/card";
 import useFetchMovies from "../hooks/useFetchMovies";
 
+
+
 function Movies() {
   const [page, setPage] = useState(1);
   const [movies, setMovies] = useState<any[]>([]);
+
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  const filteredMovies = selectedGenre
+  ? movies.filter((movie) =>
+      movie.genre_ids?.includes(Number(selectedGenre))
+    )
+  : movies;
 
   const { data, loading, error } = useFetchMovies({
     url: `/movie/popular?page=${page}`,
@@ -23,10 +33,27 @@ function Movies() {
 
   return (
     <div className="movies-container px-4">
+      <div className="movies-header">
       <h2 className="section-title">All Movies</h2>
 
+      <select
+  className="genre-dropdown"
+  value={selectedGenre}
+  onChange={(e) => setSelectedGenre(e.target.value)}
+>
+  <option value="">All Genres</option>
+  <option value="28">Action</option>
+  <option value="35">Comedy</option>
+  <option value="18">Drama</option>
+  <option value="27">Horror</option>
+  <option value="10749">Romance</option>
+</select>
+</div>
+
+
+
       <div className="movies-grid">
-        {movies.map(movie => (
+        {filteredMovies.map(movie => (
           <MovieCard
             key={movie.id}
             id={movie.id}
